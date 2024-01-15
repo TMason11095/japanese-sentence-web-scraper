a944484f4f20eccab1d64a7baffbded96fb8f10c
TMason11095 <tmason11095@gmail.com>
2024-01-14 20:08:34 -0600

    Moved example sentence logic into its own file.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import * as helper from './js/helper.js';
+import * as exampleSentences from './js/example_sentences.js';
 
 import * as error from 'console';
 import * as puppeteer from 'puppeteer';
@@ -23,38 +24,15 @@ const currentUrl = mainExampleUrl;
         const page = await browser.newPage();
         //Load cookies
         await helper.loadCookiesFromFile(page, cookiesDirPath + "kanshudo.json");
-        //Navigate to the Kanshudo website
-        await page.goto(currentUrl);
-        //Inject jQuery
-            //await helper.injectJquery(page);
-        //Save cookies
-            //await helper.saveCookiesToFile(page, cookiesDirPath + "kanshudo.json");
-        //Save HTML
-            //await helper.saveHtmlToFile(page, htmlDirPath + "kanshudo.html");
-        //Get the Japanese example sentence breakdown
-        const japSentence = await getJapSentence(page);
-        //Get the English translation of the example sentence
-        const engSentence = await getEngSentence(page);
-        //Get the grammar ids
-        const grammarIds = await getGrammarIds(page);
-        //Get vocab ids
-        const vocabIds = await getIds(page, '#main-content .bodyarea .jukugorow', 'id', '^(jukugo_)');
-        //Get kanji ids
-        const kanjiIds = await getIds(page, '#main-content .bodyarea div .kanjirow.level0 .kr_container .kanji', 'id', '^(k_kan_)');
-        
-        //Merge the objects
-        const mergedObjects = {
-            japanese_sentence: japSentence,
-            english_sentence: engSentence,
-            grammar_ids: grammarIds,
-            vocab_ids: vocabIds,
-            kanji_ids: kanjiIds
-        };
+        //Get example sentence object
+        const exampleSentence = await exampleSentences.getExampleSentence(page, currentUrl);
 
-        const mergedObjectsJson = JSON.stringify(mergedObjects, null, 4);
-        
+
+
+        //Convert example sentence to JSON
+        const exampleSentenceJson = JSON.stringify(exampleSentence, null, 4);
         //Save Sentence JSON to file
-        await helper.saveDataToFile(mergedObjectsJson, jsonDirPath + "test7.json");
+        await helper.saveDataToFile(exampleSentenceJson, jsonDirPath + "test8.json");
 
         //Take a screenshot of the current page
             //await page.screenshot({path: screenshotsDirPath + "page.png", fullPage: true});
@@ -73,136 +51,3 @@ const currentUrl = mainExampleUrl;
     }
     
 })();
-
-//Grammar is split up into 2 areas. Need to combine them here
-async function getGrammarIds(page) {
-    //Get the standard grammar ids
-    const standardGrammarIds = await getIds(page, '#main-content .bodyarea div .gp_search .gp_icons .jlpt_container', 'id', '^(jlpt_)');
-    //Get inflection ids
-    const inflectionIds = await getIds(page, '#main-content .bodyarea div .gp_search a[href^="/grammar/id/"]', 'href', '^(https://www.kanshudo.com/grammar/id/)');
-    //Merge the 2 id lists to get all grammar ids (might have duplicate entries)
-    const dupGrammarIds = standardGrammarIds.concat(inflectionIds);
-    //Remove duplicate entries by converting the array to a set and then back to an array
-    const grammarIds = Array.from(new Set(dupGrammarIds));
-    //Return
-    return grammarIds;
-}
-
-async function getIds(page, entriesSelector, entryIdField, idPrefixRegExText) {
-    //Get ids
-    const ids = await page.evaluate((entriesSelector, entryIdField, idPrefixRegExText) => {
-        //Get id entries
-        const entries = document.querySelectorAll(entriesSelector);
-        //Loop and grab ids
-        let ids = [];
-        for (const entry of entries) {
-            //Get prefixed id
-            const prefixedId = entry[entryIdField];
-            //Remove prefix
-            const id = prefixedId.replace(new RegExp(idPrefixRegExText), "");
-            //Add to list
-            ids.push(id);
-        }
-        //Return
-        return ids;
-    }, entriesSelector, entryIdField, idPrefixRegExText);
-    //Return
-    return ids;
-}
-
-async function getEngSentence(page) {
-    //Grab the example sentence section
-    const engSentenceElement = await page.$('#main-content .bodyarea .spaced .tatoeba .tat_eng .text');
-    //Grab the text
-    const engSentence = await page.evaluate((engSentenceElement) => engSentenceElement.textContent, engSentenceElement);
-    //Return
-    return engSentence;
-}
-
-async function getJapSentence(page) {
-    //Get and process Japanese sentence
-    const japSentence = await page.evaluate(() => {
-        //Get sentence elements
-        const sentenceElements = document.querySelectorAll('#main-content .bodyarea .spaced .tatoeba [class*="tatvoc"]');
-        //Loop through and build the sentence
-        let japSentenceBuilder = [];
-        for (const element of sentenceElements) {
-            switch (element.tagName) {
-                case 'A'://Clickable (vocab)
-                    //Track if kanji was found to put it in a kanji_vocab object
-                    let hasKanji = false;
-                    //Loop through children and build the vocab
-                    let vocabBuilder = [];
-                    for (const node of element.childNodes) {
-                        //Check the node type
-                        switch (node.nodeType) {
-                            case Node.ELEMENT_NODE: //Kanji with furigana
-                                //Set kanji bool
-                                hasKanji = true;
-                                //Add to vocab
-                                vocabBuilder.push({
-                                    kanji: node.querySelector('.f_kanji').textContent,
-                                    furigana: node.querySelector('.furigana').textContent
-                                });
-                                break;
-                            case Node.TEXT_NODE: //Hiragana
-                                //Add to vocab
-                                vocabBuilder.push(node.textContent);
-                                break;
-                        }
-                    }
-                    //Set vocab (overridable for kanji_vocab)
-                    let vocab = vocabBuilder;
-                    //Check if the entry was kanji_vocab
-                    if (hasKanji) {
-                        //Set vocab as an object
-                        vocab = { kanji_vocab: vocabBuilder };
-                    }
-                    //Add the vocab to the sentence
-                    japSentenceBuilder.push(vocab);
-                    break;
-                case 'SPAN'://Punctuation and joining characters
-                    //Add to the sentence
-                    japSentenceBuilder.push(element.textContent);
-                    break;
-            }
-        }
-        //Combine from sentence builder
-        const japSentence = japSentenceBuilder.flat();
-        //Merge any non kanji_vocab that are in a row
-        let mergedJapSentence = [];
-        let nonKanjiCount = 0;
-        let mergedText = "";
-        for (const entry of japSentence) {
-            //Check if kanji_vocab
-            if (entry.kanji_vocab) {
-                //Check if there was a previous non kanji entry
-                if (nonKanjiCount > 0) {
-                    //Add the merged entries
-                    mergedJapSentence.push(mergedText);
-                    //Reset the count
-                    nonKanjiCount = 0;
-                    //Reset the merge text
-                    mergedText = "";
-                }
-                //Add kanji_vocab
-                mergedJapSentence.push(entry);
-            }
-            else {
-                //Increment counter
-                nonKanjiCount++;
-                //Add text
-                mergedText += entry;
-            }
-        }
-        //Check if there was any non kanji entries at the end
-        if (nonKanjiCount > 0) {
-            //Add the merged entries
-            mergedJapSentence.push(mergedText);
-        }
-        //Return the sentence
-        return mergedJapSentence;
-    });
-    //Return the sentence
-    return japSentence;
-}
---
+++ js/example_sentences.js
@@ -0,0 +1,163 @@
+export async function getExampleSentence(page, exampleUrl) {
+    //Navigate to the Kanshudo website
+    await page.goto(exampleUrl);
+    //Save cookies
+        //await helper.saveCookiesToFile(page, cookiesDirPath + "kanshudo.json");
+    //Save HTML
+        //await helper.saveHtmlToFile(page, htmlDirPath + "kanshudo.html");
+    //Get the Japanese example sentence breakdown
+    const japSentence = await getJapSentence(page);
+    //Get the English translation of the example sentence
+    const engSentence = await getEngSentence(page);
+    //Get the grammar ids
+    const grammarIds = await getGrammarIds(page);
+    //Get vocab ids
+    const vocabIds = await getIds(page, '#main-content .bodyarea .jukugorow', 'id', '^(jukugo_)');
+    //Get kanji ids
+    const kanjiIds = await getIds(page, '#main-content .bodyarea div .kanjirow.level0 .kr_container .kanji', 'id', '^(k_kan_)');
+    
+    //Merge the objects
+    const mergedObjects = {
+        japanese_sentence: japSentence,
+        english_sentence: engSentence,
+        grammar_ids: grammarIds,
+        vocab_ids: vocabIds,
+        kanji_ids: kanjiIds
+    };
+
+    //Return
+    return mergedObjects;
+}
+
+//Grammar is split up into 2 areas. Need to combine them here
+async function getGrammarIds(page) {
+    //Get the standard grammar ids
+    const standardGrammarIds = await getIds(page, '#main-content .bodyarea div .gp_search .gp_icons .jlpt_container', 'id', '^(jlpt_)');
+    //Get inflection ids
+    const inflectionIds = await getIds(page, '#main-content .bodyarea div .gp_search a[href^="/grammar/id/"]', 'href', '^(https://www.kanshudo.com/grammar/id/)');
+    //Merge the 2 id lists to get all grammar ids (might have duplicate entries)
+    const dupGrammarIds = standardGrammarIds.concat(inflectionIds);
+    //Remove duplicate entries by converting the array to a set and then back to an array
+    const grammarIds = Array.from(new Set(dupGrammarIds));
+    //Return
+    return grammarIds;
+}
+
+async function getIds(page, entriesSelector, entryIdField, idPrefixRegExText) {
+    //Get ids
+    const ids = await page.evaluate((entriesSelector, entryIdField, idPrefixRegExText) => {
+        //Get id entries
+        const entries = document.querySelectorAll(entriesSelector);
+        //Loop and grab ids
+        let ids = [];
+        for (const entry of entries) {
+            //Get prefixed id
+            const prefixedId = entry[entryIdField];
+            //Remove prefix
+            const id = prefixedId.replace(new RegExp(idPrefixRegExText), "");
+            //Add to list
+            ids.push(id);
+        }
+        //Return
+        return ids;
+    }, entriesSelector, entryIdField, idPrefixRegExText);
+    //Return
+    return ids;
+}
+
+async function getEngSentence(page) {
+    //Grab the example sentence section
+    const engSentenceElement = await page.$('#main-content .bodyarea .spaced .tatoeba .tat_eng .text');
+    //Grab the text
+    const engSentence = await page.evaluate((engSentenceElement) => engSentenceElement.textContent, engSentenceElement);
+    //Return
+    return engSentence;
+}
+
+async function getJapSentence(page) {
+    //Get and process Japanese sentence
+    const japSentence = await page.evaluate(() => {
+        //Get sentence elements
+        const sentenceElements = document.querySelectorAll('#main-content .bodyarea .spaced .tatoeba [class*="tatvoc"]');
+        //Loop through and build the sentence
+        let japSentenceBuilder = [];
+        for (const element of sentenceElements) {
+            switch (element.tagName) {
+                case 'A'://Clickable (vocab)
+                    //Track if kanji was found to put it in a kanji_vocab object
+                    let hasKanji = false;
+                    //Loop through children and build the vocab
+                    let vocabBuilder = [];
+                    for (const node of element.childNodes) {
+                        //Check the node type
+                        switch (node.nodeType) {
+                            case Node.ELEMENT_NODE: //Kanji with furigana
+                                //Set kanji bool
+                                hasKanji = true;
+                                //Add to vocab
+                                vocabBuilder.push({
+                                    kanji: node.querySelector('.f_kanji').textContent,
+                                    furigana: node.querySelector('.furigana').textContent
+                                });
+                                break;
+                            case Node.TEXT_NODE: //Hiragana
+                                //Add to vocab
+                                vocabBuilder.push(node.textContent);
+                                break;
+                        }
+                    }
+                    //Set vocab (overridable for kanji_vocab)
+                    let vocab = vocabBuilder;
+                    //Check if the entry was kanji_vocab
+                    if (hasKanji) {
+                        //Set vocab as an object
+                        vocab = { kanji_vocab: vocabBuilder };
+                    }
+                    //Add the vocab to the sentence
+                    japSentenceBuilder.push(vocab);
+                    break;
+                case 'SPAN'://Punctuation and joining characters
+                    //Add to the sentence
+                    japSentenceBuilder.push(element.textContent);
+                    break;
+            }
+        }
+        //Combine from sentence builder
+        const japSentence = japSentenceBuilder.flat();
+        //Merge any non kanji_vocab that are in a row
+        let mergedJapSentence = [];
+        let nonKanjiCount = 0;
+        let mergedText = "";
+        for (const entry of japSentence) {
+            //Check if kanji_vocab
+            if (entry.kanji_vocab) {
+                //Check if there was a previous non kanji entry
+                if (nonKanjiCount > 0) {
+                    //Add the merged entries
+                    mergedJapSentence.push(mergedText);
+                    //Reset the count
+                    nonKanjiCount = 0;
+                    //Reset the merge text
+                    mergedText = "";
+                }
+                //Add kanji_vocab
+                mergedJapSentence.push(entry);
+            }
+            else {
+                //Increment counter
+                nonKanjiCount++;
+                //Add text
+                mergedText += entry;
+            }
+        }
+        //Check if there was any non kanji entries at the end
+        if (nonKanjiCount > 0) {
+            //Add the merged entries
+            mergedJapSentence.push(mergedText);
+        }
+        //Return the sentence
+        return mergedJapSentence;
+    });
+    //Return the sentence
+    return japSentence;
+}
